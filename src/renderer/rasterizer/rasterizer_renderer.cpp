de1cb7f452a6c23cafa9b88625d1967d059a1657
Ghadeer <ghadeer_akleh@hotmail.com>
2022-06-09 23:11:20 +0300

    Code editing

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -39,7 +39,7 @@ void cg::renderer::rasterization_renderer::init()
 }
 void cg::renderer::rasterization_renderer::render()
 {
-	rasterizer->clear_render_target({112, 15, 115});
+	rasterizer->clear_render_target({255, 229, 153});
 
 	//Lab 1.04. Implement `vertex_shader` lambda for the instance of `cg::renderer::rasterizer`
 
