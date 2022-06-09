0154a5ecbafe0c0335bafe95c166548c50a4a906
Ghadeer <ghadeer_akleh@hotmail.com>
2022-06-09 14:56:37 +0300

    Lab 1.06. Add depth buffer in cg::renderer::rasterization_renderer

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -10,11 +10,11 @@ void cg::renderer::rasterization_renderer::init()
 	rasterizer = std::make_shared<cg::renderer::rasterizer<cg::vertex, cg::unsigned_color>>();
 	rasterizer->set_viewport(settings->width, settings->height);
 
-	// TODO: Lab 1.03. Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
+	//Lab 1.03. Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
 	model = std::make_shared<cg::world::model>();
 	model->load_obj(settings->model_path);
 
-	// TODO: Lab 1.04. Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`
+	//Lab 1.04. Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`
 	camera = std::make_shared<cg::world::camera>();
 	camera->set_height(static_cast<float>(settings->height));
 	camera->set_width(static_cast<float>(settings->width));
@@ -30,7 +30,7 @@ void cg::renderer::rasterization_renderer::init()
 	camera->set_angle_of_view(settings->camera_angle_of_view);
 	camera->set_z_near(settings->camera_z_near);
 	camera->set_z_far(settings->camera_z_far);
-	// TODO: Lab 1.06. Add depth buffer in cg::renderer::rasterization_renderer
+	//Lab 1.06. Add depth buffer in cg::renderer::rasterization_renderer
 
 	depth_buffer = std::make_shared<cg::resource<float>>(
 			settings->width, settings->height);
